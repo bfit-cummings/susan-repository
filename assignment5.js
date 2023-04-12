e767c91ca3b0b21c52cbc7c801308d9863ad7329
susanc0829 <schen3056@ben.bfit.edu>
2023-04-12 17:22:35 -0400

    Create assignment5.js

---
+++ assignment5.js
@@ -0,0 +1,6 @@
+const login = [{username: 'Susan', age: 16, password: 'hi:)'},
+{username: 'Evelyn', age: 16, password: 'yourmom'},
+{username: 'Andrew', age: 17, password: 'yourdad'},
+{username: 'Michael', age: 17, password: 'joke2030'}]
+
+login.push({username: 'Steven', age: 29, password: 'cheese'})
