a723cff3f5ed08731409940238bf0502cf42ce95
susanc0829 <schen3056@ben.bfit.edu>
2023-04-12 18:25:20 -0400

    Update assignment5.js

--- assignment5.js
+++ assignment5.js
@@ -1,15 +1,16 @@
-const login = [{username: 'Susan', age: 16, password: 'hi:)'},
-{username: 'Evelyn', age: 16, password: 'yourmom'},
-{username: 'Andrew', age: 17, password: 'yourdad'},
-{username: 'Michael', age: 17, password: 'joke2030'}]
+const login = [{id: 1, username: 'Susan', age: 16, password: 'hi:)'},
+{id: 2, username: 'Evelyn', age: 16, password: 'yourmom'},
+{id: 3, username: 'Andrew', age: 17, password: 'yourdad'},
+{id: 4, username: 'Michael', age: 17, password: 'joke2030'}]
 
-login.push({username: 'Steven', age: 29, password: 'cheese'})
+login.push({id: 5, username: 'Steven', age: 29, password: 'cheese'})
 console.log(login)
 
 while (true) {
+    const id = login.length + 1
     const username = prompt('Username: ')
     const age = prompt('age: ')
     const password = prompt('password: ')
-    login.push ({username: username, age: age, password: password})
+    login.push ({id: id, username: username, age: age, password: password})
     console.log(login)
 }
